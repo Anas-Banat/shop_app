85047a2b07aeb2ed2b16e9ca64d04a9ff7e8a47b
ANAS BANAT <anasbanat.93.ab@gmail.com>
2023-03-19 00:21:13 +0300

    new update

--- lib/modules/shop_app/login/shop_login.dart
+++ lib/modules/shop_app/login/shop_login.dart
@@ -7,6 +7,7 @@ import 'package:shop_app/modules/shop_app/register_screen/shop_register_screen.d
 import 'package:shop_app/shared/components/components.dart';
 
 class ShopLoginScreen extends StatelessWidget {
+  var formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -19,82 +20,91 @@ class ShopLoginScreen extends StatelessWidget {
       child: BlocConsumer<ShopLoginCubit, ShopLoginStates>(
         listener: (context, state) {},
         builder: (context, state) {
-           Scaffold(
+           return Scaffold(
             appBar: AppBar(),
             body: Center(
               child: SingleChildScrollView(
                 child: Padding(
                   padding: const EdgeInsets.all(20.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('LOGIN', style: Theme.of(context).textTheme.titleLarge.copyWith(
-                        color: Colors.black, 
-                      ),),
-                      SizedBox(
-                        height: 20.0,
-                      ),
-                      Text('Login now to browse our hot offers', style: Theme.of(context).textTheme.titleMedium.copyWith(
-                        color: Colors.grey,
-                      ),),
-                      SizedBox(
-                        height: 30.0,
-                      ),
-                      defaultFormField(
-                        controller: emailController,
-                        type: TextInputType.emailAddress,
-                        validate: (String value){
-                          if(value.isEmpty){
-                            return 'Please enter your email address';
-                          }
-                        },
-                        lable: 'Email Address',
-                        prefix: Icons.email_outlined,
-                        
-                      ),
-                      SizedBox(
-                        height: 15.0,
-                      ),
-                      defaultFormField(
-                        controller: passController,
-                        type: TextInputType.visiblePassword,
-                        suffix: Icons.visibility_outlined,
-                        validate: (String value){
-                          if(value.isEmpty){
-                            return 'Password is too short';
-                          }
-                        },
-                        lable: 'Password',
-                        prefix: Icons.lock_outline,
-                      ),
-                      SizedBox(
-                        height: 40,
-                      ),
-                      ConditionalBuilder(
-                        condition: false,
-                        builder: (context) => defaultButton(
-                        text: 'Login',
-                        isUpperCase: true,
-                        function: (){},
+                  child: Form(
+                    key: formKey,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('LOGIN', style: Theme.of(context).textTheme.titleLarge.copyWith(
+                          color: Colors.black, 
+                        ),),
+                        SizedBox(
+                          height: 20.0,
                         ),
-                        fallback: (context) => CircularProgressIndicator(),
-                      ),
-                      SizedBox(
-                        height: 15.0,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text('don\'t have an account?',),
-                          defaultTextButton(
-                            function: (){
-                              navigateTo(context, RegisterScreen());
-                            },
-                            text: 'Register',
-                          ),
-                        ],
-                      ),
-                    ],
+                        Text('Login now to browse our hot offers', style: Theme.of(context).textTheme.titleMedium.copyWith(
+                          color: Colors.grey,
+                        ),),
+                        SizedBox(
+                          height: 30.0,
+                        ),
+                        defaultFormField(
+                          controller: emailController,
+                          type: TextInputType.emailAddress,
+                          validate: (String value){
+                            if(value.isEmpty){
+                              return 'Please enter your email address';
+                            }
+                          },
+                          lable: 'Email Address',
+                          prefix: Icons.email_outlined,
+                          
+                        ),
+                        SizedBox(
+                          height: 15.0,
+                        ),
+                        defaultFormField(
+                          controller: passController,
+                          type: TextInputType.visiblePassword,
+                          suffix: Icons.visibility_outlined,
+                          validate: (String value){
+                            if(value.isEmpty){
+                              return 'Password is too short';
+                            }
+                          },
+                          lable: 'Password',
+                          prefix: Icons.lock_outline,
+                        ),
+                        SizedBox(
+                          height: 40,
+                        ),
+                        ConditionalBuilder(
+                          condition: state is! ShopLoginLoadinglState,
+                          builder: (context) => defaultButton(
+                          text: 'Login',
+                          isUpperCase: true,
+                          function: (){
+                            if(formKey.currentState.validate()){
+                              ShopLoginCubit.get(context).userLogin(
+                              email: emailController.text,
+                              password: passController.text,
+                              );
+                            }
+                          }),
+                          fallback: (context) => Center(child: CircularProgressIndicator()),
+                        ),
+                        SizedBox(
+                          height: 15.0,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text('don\'t have an account?',),
+                            defaultTextButton(
+                              function: (){
+                                navigateTo(context, RegisterScreen());
+                              },
+                              text: 'Register',
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
