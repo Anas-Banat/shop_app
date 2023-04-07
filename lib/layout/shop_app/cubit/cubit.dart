535050ed689e744b26c07755b240e691e7c89a1e
ANAS BANAT <anasbanat.93.ab@gmail.com>
2023-04-07 14:14:27 +0300

    new update

--- lib/layout/shop_app/cubit/cubit.dart
+++ lib/layout/shop_app/cubit/cubit.dart
@@ -4,4 +4,5 @@ import 'package:shop_app/layout/shop_app/cubit/states.dart';
 class ShopCubit extends Cubit<ShopCubit>{
   ShopCubit() : super(ShopInitialState() as ShopCubit);
   static ShopCubit get(context) => BlocProvider.of(context);
+  int currentIndex = 0;
 }
