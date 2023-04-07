943a061d908394722b13e9a5e713340c375ca245
ANAS BANAT <anasbanat.93.ab@gmail.com>
2023-04-07 14:19:30 +0300

    new update

---
+++ lib/modules/shop_app/favorites/favorits_screen.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class FavoritsScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Text('Favorits Screen', style: Theme.of(context).textTheme.bodyLarge,);
+  }
+}
--- lib/modules/shop_app/products/products_screen.dart
+++ lib/modules/shop_app/products/products_screen.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 class ProductsScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Text('Products Screen', style: Theme.of(context).textTheme.bodyLarge,);
   }
-
 }
